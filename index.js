9ec2bb158dcc170f89069db9e1bbcfe1b8e597b0
berdi00 <berdinazarov731@gmail.com>
2023-09-25 20:15:44 +0500

    update

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-const { Printer } = require("@node-escpos/core");
 const express = require("express");
 const USB = require("@node-escpos/usb-adapter");
 const app = express();
 
+const device = new USB();
 app.get("/print", async (req, res) => {
-  let device;
-
   try {
-    // give VID and PID
-    device = new USB();
-  } catch (error) {
-    Promise.reject(err);
-    throw new Error("Couldn't define printer");
+    await device.open();
+    await device.print("Hello, World!");
+    await device.cut();
+    await device.close();
+    res.send("Printing successful!");
+  } catch {
+    console.error("Error printing receipt:", error);
+    res.status(500).send("Error printing receipt");
   }
-
-  device.open(async function (err) {
-    if (err) {
-      Promise.reject(err);
-      throw new Error("Couldn't open printer");
-    }
-    const options = { encoding: "ISO-8859-9" };
-    const printer = new Printer(device, options);
-    printer.size(2, 2).align("rt").text("hello");
-    printer.feed().feed().feed().align("lt");
-    printer.buffer.write(Buffer.from([0x1b, 0x69]));
-    try {
-      await printer.close();
-    } catch (err) {}
-  });
-  return res.send("Printed successfully");
 });
 
 app.listen(2999, () => {
